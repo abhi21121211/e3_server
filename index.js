301dc5bdccbc073a11411133cd8591d2b4806949
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2023-07-31 23:25:38 +0530

    das

--- index.js
+++ index.js
@@ -1,80 +1,76 @@
-const express = require("express")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-require("dotenv").config()
-const cors = require("cors")
+const express = require('express');
 
-const { connection } = require("./config/db")
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors')
+
+const { connection } = require("./config/db");
 const { UserModel } = require("./models/User.model")
-const { blogRouter } = require("./routes/blog.routes")
-const { authentication } = require("./middlewares/authentication")
+const {authentication}=require("./middleware/authentication")
+const {blogRouter}=require("./routes/blog.routes")
+require("dotenv").config();
 
 
-const app = express()
+
+const app = express();
+
+
 app.use(cors({
     origin: "*"
 }))
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Base API endpoint")
+app.get("/", async (req, res) => {
+    res.json("welcome to blogs application")
 })
 
+
 app.post("/signup", async (req, res) => {
-    let { name, email, password, age, phone_number } = req.body;
+    let { name, email, password } = req.body
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const new_user = new UserModel({
-            name,
-            email,
-            password: hashedPassword,
-            age,
-            phone_number,
-        });
+        const hashedPassword = await bcrypt.hash(password, 5);
+        const new_user = new UserModel({ name, email, password: hashedPassword });
         await new_user.save();
-        res.send({ msg: "sign up successful" });
-    } catch (err) {
-        console.log("Signup error:", err);
-                res.status(500).send("Something went wrong, please try again later");
+        res.json({ msg: "signup successful" })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Something went wrong. Please try again later")
     }
-});
+})
 
 
 app.post("/login", async (req, res) => {
-    const { email, password } = req.body; //elon@123
+    let { email, password } = req.body
+
+
     const user = await UserModel.findOne({ email })
     if (!user) {
         res.send("Sign up first")
-    }
-    else {
-        const hashed_password = user.password
-        bcrypt.compare(password, hashed_password, function (err, result) {
+    } else {
+        const hashedPassword = user.password
+        bcrypt.compare(password, hashedPassword, function (err, result) {
             if (result) {
-                let token = jwt.sign({ user_id: user._id }, process.env.SECRET_KEY);
-                res.send({ msg: "login successfull", token: token })
+                let token = jwt.sign({ user_id: user._id }, process.env.SECRET_KEY)
+                res.send({ msg: "login success", token: token })
             }
             else {
-                res.send("Login failed, invalid credentials")
+                res.json("login failed invalid credentials")
             }
-        });
+        })
     }
+
 })
 
-
-app.use("/blogs", authentication, blogRouter)
-
+app.use("/blogs",authentication,blogRouter)
 
 app.listen(process.env.PORT, async () => {
     try {
         await connection
-        console.log("connected to DB Successfully")
+        console.log("connected to db server")
+    } catch (error) {
+        console.log(error)
+        console.log("error connecting to db server")
     }
-    catch (err) {
-        console.log("error while connecting to DB")
-        console.log(err)
-    }
-    console.log("listening on port 8000")
+    console.log("listening on 8000 port server")
 })
-
-
